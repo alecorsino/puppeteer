52ab9d5157a807e8e04d6be2d95ced0c1ca3a4ae
Alejandro Corsino <alecorsino@gmail.com>
2017-12-24 16:49:54 -0800

    Build and start scripts

---
+++ bin/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo [BUILDING DOCKER IMAGE...
+
+docker build -t puppeteer]
---
+++ bin/start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo [RUNNING CONTAINER]
+
+docker run -it puppeteer /bin/bash
